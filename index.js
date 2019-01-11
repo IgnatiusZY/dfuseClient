6bf354bba670f82f786df83a2de93fd96fabbb4b
ignatiusrevi <ignatiusrevi38@gmail.com>
2019-01-11 16:13:41 +1100

    added travis file

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 const token = "eyJhbGciOiJLTVNFUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDk1MjAzMTIsImp0aSI6Ijk4ODIxYmI4LWVhMmQtNDNlYS04M2ViLTJmMGI4NDY1MmFhMCIsImlhdCI6MTU0NjkyODMxMiwiaXNzIjoiZGZ1c2UuaW8iLCJzdWIiOiJDaVFBNmNieWU1czFpTkFwU0RUZEtLNHZFRUpUNlYvNHU3VE85QlpTR1ZCbjd4d0RuZmNTUGdBL0NMUnR3VEJDdmtqZHdSRmphbjAzY1ErMWpGQ05wOU84UDgzd0V2NFhRM29hOW53TWJieU14anozdlo3S01vN0Q0bXlYYXdjdTNDZGlOZkRzIiwidGllciI6ImJldGEtdjEiLCJ2IjoxfQ.GLbcuQ32cpyZUw1O0qNsMOMngwxeeV-5PQetFFyZ0bvAEcbwm2RQpHslUCqf0AkG6eCfMEpdB2SZMqpScqDGpg"
 const client = new EoswsClient(
     createEoswsSocket(
-        () => new WebSocket(`wss://${endpoint}/v1/stream?token=${token}`, { origin: "https://example.com" })
+        () => new WebSocket(`wss://${endpoint}/v1/stream?token=${token}`, { origin: "https://emanate.live" })
     )
 )
 
@@ -41,7 +41,9 @@ client
             .onMessage((message) => {
                 if (action == "transfer" && message.type == InboundMessageType.ACTION_TRACE) {
                     const { from, to, quantity, memo } = message.data.trace.act.data
-                    console.log(from, to, quantity, memo)
+                    if (to == "emanateaudio") {
+                        console.log(from, to, quantity, memo)                        
+                    }
                 }
             })
     })
